a0e8cccc5661bcfa95719af3f51f783ff6916fcc
Brandon Rivera-Melo <brm@alum.northwestern.edu>
2019-05-08 00:39:36 -0700

    Split source into multiple projects for CRP adherence-- separated interfaces from implementations Added dependency on eventbroker interfaces for use with new class-- BlackboardWithNotifications

--- IBlackboard/IBlackboard.cs
+++ IBlackboard/IBlackboard.cs
@@ -1,10 +1,7 @@
-﻿using System;
-
-namespace BRM.Blackboards.Interfaces
+﻿namespace BRM.Blackboards.Interfaces
 {
     /// <summary>
     /// Allows CRUD operations (create, read, update, delete) for global variables mapped with a key,value pairing
-    /// Also provides notification for when values are updated
     /// </summary>
     public interface IBlackboard
     {
@@ -33,25 +30,8 @@ namespace BRM.Blackboards.Interfaces
         void Delete(string key);
         
         /// <summary>
-        /// Stores a callback "onUpdate", which will fire with the updated value when the "post" method is called
-        /// DO NOT pass anonymous methods for "onUpdate", as these will likely causes persistent references to objects
-        /// captured in the anonymous method closure and create memory leaks in your application
-        /// If the type T changes for a given key, event processors subscribed to value updates will remain subscribed,
-        /// but they will not receive the update events, as the types no longer match.
-        /// Changing types for a given key is not advised, as this will likely cause unexpected behavior
-        /// Null or empty keys will be ignored
-        /// </summary>
-        void Subscribe<T>(string key, Action<T> onUpdate);
-        
-        /// <summary>
-        /// Clears the callback paired with the given key
-        /// Null or empty keys will be ignored
-        /// </summary>
-        void Unsubscribe<T>(string key, Action<T> onUpdate);
-
-        /// <summary>
         /// Clears all data in the blackboard
         /// </summary>
-        void Clear();
+        void ClearData();
     }
 }
